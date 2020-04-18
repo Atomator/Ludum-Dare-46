096844d16cac9b7237769c74c674bc3ee695b7a6
ejnAjaK3VgnnHBLk <charles.igel@s.stemk12.org>
2020-04-18 00:53:07 -0600

    added movement and camera tracking

---
+++ Assets/MoveTempCharacter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTempCharacter : MonoBehaviour
+{
+    public float speed = 100;
+    public Rigidbody2D rb;
+
+    public void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 tempVect = new Vector3(h, v, 0);
+        tempVect = tempVect.normalized * speed * Time.deltaTime;
+        rb.MovePosition(rb.transform.position + tempVect);
+    }
+    
+    /* Another solution potentially better for detecting jumps and crouch and whatnot
+     * public void Update()
+     * {
+     *      if (Input.GetKey(KeyCode.A))
+     *          // A key has been pressed
+     *      if (Input.GetKey(KeyCode.D))
+     *          // D key has been pressed
+     *      if (Input.GetKey(KeyCode.W))
+     *          // W key has been pressed
+     *      if (Input.GetKey(KeyCode.S))
+     *          // S key has been pressed
+     * }
+     * Could use rb.AddForce(Vector3.<left/right>);
+     */
+}

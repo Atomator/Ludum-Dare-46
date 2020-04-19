91719780ece0d8aa0a4d56814755d2eacf491ee9
Atomator <30665220+Atomator@users.noreply.github.com>
2020-04-19 02:30:57 -0600

    Kind of a game

--- Assets/Scripts/Prefabs/Fireball/FireballCollision.cs
+++ Assets/Scripts/Prefabs/Fireball/FireballCollision.cs
@@ -7,15 +7,20 @@ public class FireballCollision : MonoBehaviour
     public float projectileDamage = 20f; 
     
     // Start is called before the first frame update
-    void OnCollisionEnter2D(Collision2D collision) {
+    void OnCollisionEnter2D(Collision2D collided) {
         Destroy(this.gameObject);
-    }
-
-    void OnTriggerEnter2D(Collider2D collider) {
-        if (collider.gameObject.tag == "Enemy") {
+        if (collided.gameObject.tag == "Enemy") {
             Destroy(this.gameObject);
-            EnemyProperties Enemy = collider.gameObject.GetComponent<EnemyProperties>();
+            EnemyProperties Enemy = collided.gameObject.GetComponent<EnemyProperties>();
             Enemy.health -= projectileDamage;
         }
     }
+
+    // void OnTriggerEnter2D(Collider2D collider) {
+    //     if (collider.gameObject.tag == "Enemy") {
+    //         Destroy(this.gameObject);
+    //         EnemyProperties Enemy = collider.gameObject.GetComponent<EnemyProperties>();
+    //         Enemy.health -= projectileDamage;
+    //     }
+    // }
 }
